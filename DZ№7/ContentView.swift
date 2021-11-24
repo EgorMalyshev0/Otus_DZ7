e04a2e5ff15f31a9475adacb05b46aac5569a2da
Egor Malyshev <egor.malyshev@umbrellait.com>
2021-11-24 18:35:55 +0300

    Initial Commit

---
+++ DZ№7/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DZ№7
+//
+//  Created by Egor Malyshev on 24.11.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DZ№7/DZ_7App.swift
@@ -0,0 +1,17 @@
+//
+//  DZ_7App.swift
+//  DZ№7
+//
+//  Created by Egor Malyshev on 24.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct DZ_7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
